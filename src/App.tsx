e0e69a31dc0a0c748e27ddb7b0812bfd580427ac
miyoshi-dev-account <miyoshi@geekfeed.co.jp>
2026-09-18 19:09:26 +0900

    deploy policy test3

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import type { Schema } from '../amplify/data/resource';
 import { generateClient } from 'aws-amplify/data';
 import ContactHistory, { ContactRecord } from './ContactHistory';
 
-// ソースコード変更によるデプロイテスト2
+// ソースコード変更によるデプロイテスト3
 
 // Cloudscapeコンポーネントを遅延ロード
 const Container = React.lazy(() => import("@cloudscape-design/components/container"));
